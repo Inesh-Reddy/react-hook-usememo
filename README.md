fc4a54a49c34ed76a4ca86168075824b6d8436cf
Inesh-Reddy <ineshreddy007@gmail.com>
2024-02-20 14:13:25 +0530

    updating use memo code

---
+++ README.md
@@ -0,0 +1,14 @@
+# UseMemo
+
+- Before we start , lets understand what memoization means
+It’s a mildly DSA concept, It means remembering some output given an input and not computing it again. {sort of caching thing}.
+
+Let's understand `UseMemo` by doing some assignments:
+
+- If I ask you to create an app that does two things :-
+
+        1. Increases a counter by 1.
+        2. Lets user put a value in an input box (n) and you need to show sum from 1-n.
+
+        Note:- Everything needs to be inside App.
+
---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { useState, useMemo } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+
+  // optimal solution: only renders when inputvalue dependency changes
+  let count = useMemo(()=>{
+    let finalCount=0;
+    for(let i=1; i<=inputValue; i++){
+      finalCount = finalCount+i;
+    }
+    return finalCount;
+  }, [inputValue])
+
+  //ugly solution: renders all the time(even if the inputvalue doesn't chage)
+  // let count = 0;
+  // for (let i = 1; i <= inputValue; i++) {
+  //   count = count + i;
+  // }
+
+  return <div>
+    <input onChange={function(e) {
+      setInputValue(e.target.value);
+    }} placeholder={"Find sum from 1 to n"}></input>
+    <br />
+    Sum from 1 to {inputValue} is {count}
+    <br />
+    <button onClick={() => {
+      setCounter(counter + 1);
+    }}>Counter ({counter})</button>
+  </div>
+}
+
+export default App;
+
+
+
+
+//another approach 
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+//   let [final, setFinal] = useState(0)
+
+//   function counter(){
+//     setCount(count+1);
+//   }
+//   function handleInputChange(event){
+//     let n = event.target.value;
+//     sum(n);
+//     // return n;
+    
+//   }
+//   function sum(n){
+//     console.log("Iam getting called e..only when counter is clicked")
+//     final =0;
+//     for(let i=1; i<=n; i++){
+//       final = final+i;
+//     }
+//     setFinal(final);
+//   }
+
+//   return (
+//     <div>
+//       <input type="text" placeholder='Number(n)' onChange={handleInputChange} /><br />
+//       <h1>Sum is {final}</h1><br />
+//       <button onClick={counter} >counter ({count})</button><br />
+//     </div>
+//   )
+// }
+
+// export default App
+
